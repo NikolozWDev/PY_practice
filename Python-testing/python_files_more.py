d31763fbb454a87ec6f714a62d5139046f4c237b
Nikoloz Gigiashvili <gigiashvilinikoloz@gmail.com>
2025-05-11 12:36:43 +0400

    files detections (writing and reading json files)

--- Python-testing/python_files_more.py
+++ Python-testing/python_files_more.py
@@ -16,8 +16,44 @@ else:
 
 # writing files
 new_file = 'files/new-file.txt'
-new_file_about = 'Get methods and more features'
+new_file_about = "Computer programming"
 
 with open(new_file, 'w') as file:
-    file.write(new_file_about)
+    i = 0
+    while i < 10:
+        file.write(new_file_about + "\n")
+        i += 1
     print(f'{new_file} was created')
+
+
+# reading files
+reader_file = 'files/reading_file.txt'
+reader_file_about = 'here is text'
+with open(reader_file, 'w') as file:
+    file.write(reader_file_about)
+    print('file created')
+try:
+    with open(reader_file, 'r') as file:
+        content = file.read()
+        print(content)
+except FileNotFoundError:
+    print('file not found')
+except PermissionError:
+    print('you have not permission to read file')
+
+
+
+import json
+
+json_file = 'files/json_about.json'
+employee = {
+    'giorgi': 'job',
+    'sandro': 'list',
+    'stefane': 'arr'
+}
+with open(json_file, 'w') as file:
+    json.dump(employee, file)
+    print('created json file')
+with open(json_file, 'r') as file:
+    content = json.load(file)
+    print(content)
