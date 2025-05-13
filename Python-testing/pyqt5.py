8bfd52ad6b2ee4ca3c6af0ed898f7e4cf7884249
Nikoloz Gigiashvili <gigiashvilinikoloz@gmail.com>
2025-05-14 00:03:20 +0400

    practicing PyQt5

--- Python-testing/pyqt5.py
+++ Python-testing/pyqt5.py
@@ -1,7 +1,8 @@
 # PyQt5 GUI intro
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtCore import Qt
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -9,6 +10,19 @@ class MainWindow(QMainWindow):
         self.setWindowTitle('main_server')
         self.setGeometry(700, 300, 500, 400)
         self.setWindowIcon(QIcon('assets/folder_92613.jpg'))
+
+        label = QLabel('Error Server', self)
+        label.setGeometry(0, 0, 500, 100)
+        label.setStyleSheet('color: red; font-size: 30px; font-weight: bold; background-color: black;')
+        label.setAlignment(Qt.AlignCenter)
+        label.setAlignment(Qt.AlignHCenter)
+
+        label2 = QLabel(self)
+        label2.setGeometry(300, 300, 500, 100)
+        pixmap = QPixmap('assets/folder_92613.jpg')
+        label2.setPixmap(pixmap)
+        label2.setScaledContents(True)
+
 
 def main():
     app = QApplication(sys.argv)
